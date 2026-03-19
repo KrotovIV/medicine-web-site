0e736aea23a4b3282483e4083619bdc621fdb493
Иван Кротов <krotowivan@yandex.ru>
2026-03-19 17:48:04 +0500

    spring dependency

--- backend/src/main/java/com/github/KrotovIV/backend/BackendApplication.java
+++ backend/src/main/java/com/github/KrotovIV/backend/BackendApplication.java
@@ -1,7 +1,11 @@
 package com.github.KrotovIV.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
-        System.out.println("Backend module started");
+        SpringApplication.run(BackendApplication.class, args);
     }
 }
--- frontend/src/main/java/com/github/KrotovIV/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/github/KrotovIV/frontend/FrontendApplication.java
@@ -1,7 +1,11 @@
 package com.github.KrotovIV.frontend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FrontendApplication {
     public static void main(String[] args) {
-        System.out.println("Frontend module started");
+        SpringApplication.run(FrontendApplication.class, args);
     }
 }
