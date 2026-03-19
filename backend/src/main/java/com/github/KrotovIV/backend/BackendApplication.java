69f37968095b819871a6fdd56e908e46de3a7965
Иван Кротов <krotowivan@yandex.ru>
2026-03-19 17:12:40 +0500

    initial project

---
+++ backend/src/main/java/com/github/KrotovIV/backend/BackendApplication.java
@@ -0,0 +1,7 @@
+package com.github.KrotovIV.backend;
+
+public class BackendApplication {
+    public static void main(String[] args) {
+        System.out.println("Backend module started");
+    }
+}
---
+++ frontend/src/main/java/com/github/KrotovIV/frontend/FrontendApplication.java
@@ -0,0 +1,7 @@
+package com.github.KrotovIV.frontend;
+
+public class FrontendApplication {
+    public static void main(String[] args) {
+        System.out.println("Frontend module started");
+    }
+}
