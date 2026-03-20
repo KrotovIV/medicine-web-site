44341c5906793a060c5b4472dd3f46b8e628d26b
Иван Кротов <krotowivan@yandex.ru>
2026-03-20 15:42:40 +0500

    delete mock in homepagecontroller

--- frontend/src/main/java/com/github/KrotovIV/frontend/controllers/HomePageController.java
+++ frontend/src/main/java/com/github/KrotovIV/frontend/controllers/HomePageController.java
@@ -40,60 +40,6 @@ public class HomePageController {
         model.addAttribute("isAuthenticated", isAuthenticated);
         model.addAttribute("username", "Пользователь");
 
-        // временно хардкод списка пациентов
-//        var patientsList = List.of(
-//            PatientCard.builder()
-//                    .avatar("👴")
-//                    .name("Иванов Иван")
-//                    .age("72 года")
-//                    .condition("Гипертония, артрит")
-//                    .lastVisitDate("\uD83E\uDE7A Последний визит: 3 дня назад")
-//                    .build(),
-//
-//            PatientCard.builder()
-//                    .avatar("\uD83D\uDC75")
-//                    .name("Петрова Мария")
-//                    .age("68 лет")
-//                    .condition("Сахарный диабет 2 типа")
-//                    .lastVisitDate("\uD83E\uDE7A Последний визит: Вчера")
-//                    .build(),
-//
-//            PatientCard.builder()
-//                    .avatar("\uD83D\uDC68")
-//                    .name("Сидоров Алексей")
-//                    .age("45 лет")
-//                    .condition("Профилактический осмотр")
-//                    .lastVisitDate("\uD83E\uDE7A Последний визит: 2 недели назад")
-//                    .build(),
-//
-//            PatientCard.builder()
-//                    .avatar("\uD83D\uDC69")
-//                    .name("Козлова Елена")
-//                    .age("34 года")
-//                    .condition("Наблюдение, здоров")
-//                    .lastVisitDate("\uD83E\uDE7A Последний визит: 1 месяц назад")
-//                    .build(),
-//
-//            PatientCard.builder()
-//                    .avatar("\uD83D\uDC74")
-//                    .name("Николаев Петр")
-//                    .age("81 год")
-//                    .condition("ИБС, ХСН")
-//                    .lastVisitDate("\uD83E\uDE7A Последний визит: 5 дней назад")
-//                    .build(),
-//
-//
-//            PatientCard.builder()
-//                    .avatar("\uD83D\uDC75")
-//                    .name("Смирнова Анна")
-//                    .age("57 лет")
-//                    .condition("Артроз, остеопороз")
-//                    .lastVisitDate("\uD83E\uDE7A Последний визит: Сегодня утром")
-//                    .build()
-//
-//        );
-
-
         // получение списка пациентов с бекенда
         var patientsList = webClient.get()
                         .uri(getPatientsListUrl)
